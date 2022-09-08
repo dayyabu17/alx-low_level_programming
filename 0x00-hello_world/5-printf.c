d6a40baf79d88ebb324aca5378708caaf20c5c2e
dayyabu17 <usmandayyabu17@gmail.com>
2022-09-08 15:42:29 -0700

    Final fix InshaAllah

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h> 
- /** 
-* main - main block 
-* Return: 0 
-*/ 
+#include <stdio.h>
+/**
+* main - The containing function of the program
+*Return: This returns 0 if the code is sucessful
+*/
 int main(void)
- {
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
- return (0); 
- }
-
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
