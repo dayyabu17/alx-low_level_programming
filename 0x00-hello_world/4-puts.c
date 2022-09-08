22243f258da20e6684db4c6fb6053aa524865a5e
dayyabu17 <usmandayyabu17@gmail.com>
2022-09-08 13:45:59 -0700

    Task 4 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+* main - The containing function of the program
+* Return: This returns 0 if the code is succesful
+*/
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
