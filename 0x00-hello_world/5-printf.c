4eadaa8180af556d59df54b66a9f3f979432817d
dayyabu17 <usmandayyabu17@gmail.com>
2022-09-08 14:19:33 -0700

    Task 5 fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h> 
  /** 
-* main - main block 
-* Return: 0 
-*/ 
-main()
- {
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
- return (0); 
+  * main - Entry point 
+  * 
+  * Return: always 0 
+  */ 
+ int main(void) 
+ { 
+                 printf("with proper grammar, but the outcome is a piece of art,\n"); 
+                         return (0); 
  }
-
