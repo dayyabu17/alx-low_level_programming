174a241e0c3e966b054646d6317d420203bb9423
dayyabu17 <usmandayyabu17@gmail.com>
2022-09-08 13:58:40 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - The containing function of the program
+* Return: returns 0 if succesful
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
